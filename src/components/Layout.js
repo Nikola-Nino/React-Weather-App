21918fa33b8abcdade8392823bb2d76ed97e1629
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2023-08-09 22:03:06 +0200

    Creating basic structure and entering some dummy data

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Layout = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Ohrid</p>
+          </div>
+          <div>
+            <h1>Celsius</h1>
+          </div>
+          <div>
+            <p>Clear</p>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <p>65 celsius</p>
+          </div>
+          <div>
+            <p>Humidity</p>
+          </div>
+          <div>
+            <p>Wind</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
