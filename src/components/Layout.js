1f64eaffc2709a5c785b2ea98a43b1fc034421a1
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2023-08-09 22:11:21 +0200

    Adding styling to layout page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,34 +1,33 @@
 import React from "react";
+import classes from "./Layout.module.css"
 
 const Layout = () => {
   return (
-    <div>
-      <div>
-        <div>
-          <div>
+      <div className={classes.container}>
+        <div className={classes.top}>
+          <div className={classes.location}>
             <p>Ohrid</p>
           </div>
-          <div>
+          <div className={classes.temp}>
             <h1>Celsius</h1>
           </div>
-          <div>
+          <div className={classes.description}>
             <p>Clear</p>
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className={classes.bottom}>
+          <div className={classes.feels}>
             <p>65 celsius</p>
           </div>
-          <div>
+          <div className={classes.humidity}>
             <p>Humidity</p>
           </div>
-          <div>
+          <div className={classes.wind}>
             <p>Wind</p>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
