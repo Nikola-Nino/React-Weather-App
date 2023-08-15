3df8d4a05fd8512fed441a4e55bd65f278489aaf
Nikola-Nino <74680506+Nikola-Nino@users.noreply.github.com>
2023-08-16 00:18:48 +0200

    Refactoring asynchronous function using async/await syntax

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,10 +10,15 @@ const Layout = () => {
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      axios.get(url).then((response) => {
-        setData(response.data);
-        console.log(response.data);
-      });
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(url);
+          setData(response.data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      fetchData();
       setLocation("");
     }
   };
